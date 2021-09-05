b59819f32f8b8b62077adc31663b80ca29455c9d
IkaWaAyuMu <pungpond4450@gmail.com>
2021-09-05 22:37:36 +0700

    Add OnClick to each button.

--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,8 +1,10 @@
+import {ClearCanvas, RandomizeAllCells} from "../stores/PixelPainterStore"
+
 const Utility = () => {
   return (
     <div className="flex justify-center space-x-3">
-      <button className="w-36">Clear</button>
-      <button className="w-36">Random color</button>
+      <button className="w-36" onClick = {ClearCanvas} >Clear</button>
+      <button className="w-36" onClick = {RandomizeAllCells}>Random color</button>
     </div>
   )
 }
